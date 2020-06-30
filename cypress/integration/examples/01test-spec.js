425668bae2f5e8b5825ad5038c3029e1e64f325c
Sacha Wharton <sacha@gmail.com>
2020-06-30 15:13:48 +0200

    learning to run cypress with different browsers, head mode and spec files

---
+++ cypress/integration/examples/01test-spec.js
@@ -0,0 +1,10 @@
+describe('My First Test Suite', function()
+{
+    it('My First Test case',function() {
+
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    } )
+
+    
+
+} )
--- cypress/integration/test01.js
+++
@@ -1,8 +0,0 @@
-describe('My First Test Suite', function()
-{
-    it('My First Test case',function() {
-
-        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-    } )
-
-} )
