801d424a397b098ef7135873ebec7c3d87dfc1fc
Sacha Wharton <sacha@gmail.com>
2020-07-01 13:16:12 +0200

    type in the search box and check that 4 products display

--- cypress/integration/examples/01test-spec.js
+++ cypress/integration/examples/01test-spec.js
@@ -1,8 +1,16 @@
+/// <reference types="Cypress" />
+
 describe('My First Test Suite', function()
 {
     it('My First Test case',function() {
 
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.get('.search-keyword').type('ca')
+        //selenium get hit url in browser, cypress get acts like FindElement in selenium
+        cy.get('.product').should('have.length',4)
+        
+
+
     } )
 
     
