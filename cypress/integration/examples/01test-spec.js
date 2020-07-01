de15f3cc8727fbda1818179db0c914234d6db401
Sacha Wharton <sacha@gmail.com>
2020-07-01 13:29:11 +0200

    visible elements only

--- cypress/integration/examples/01test-spec.js
+++ cypress/integration/examples/01test-spec.js
@@ -5,9 +5,13 @@ describe('My First Test Suite', function()
     it('My First Test case',function() {
 
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-        cy.get('.search-keyword').type('ca')
+        //typing in the search box
+        cy.get('.search-keyword').type('ca');
+        //adding a hard wait of 2s for the products to load properly
+        cy.wait(2000);
         //selenium get hit url in browser, cypress get acts like FindElement in selenium
-        cy.get('.product').should('have.length',4)
+        //display 4 products
+        cy.get('.product:visible').should('have.length',4);
         
 
 
