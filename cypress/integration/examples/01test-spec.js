874b6fcdc778a220b4f0edf083d165f1d8d212bc
Sacha Wharton <sacha@gmail.com>
2020-07-01 13:45:42 +0200

    adding the 2nd product to the cart

--- cypress/integration/examples/01test-spec.js
+++ cypress/integration/examples/01test-spec.js
@@ -9,10 +9,13 @@ describe('My First Test Suite', function()
         cy.get('.search-keyword').type('ca');
         //adding a hard wait of 2s for the products to load properly
         cy.wait(2000);
-        //selenium get hit url in browser, cypress get acts like FindElement in selenium
+        //selenium get hits the url in browser, cypress get acts like FindElement in selenium
         //display 4 products and visible only
         cy.get('.product:visible').should('have.length',4);
-        
+        //parent child chaining
+        cy.get('.products').find('.product').should('have.length',4);
+        //add two to the cart for the second product
+        cy.get('.products').find('.product').eq(2);
 
 
     } )
