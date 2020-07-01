fa6b7a53f6b239db7867032aaf1e643ab5f77f63
Sacha Wharton <sacha@gmail.com>
2020-07-01 13:47:06 +0200

    selecting text

--- cypress/integration/examples/01test-spec.js
+++ cypress/integration/examples/01test-spec.js
@@ -15,7 +15,7 @@ describe('My First Test Suite', function()
         //parent child chaining
         cy.get('.products').find('.product').should('have.length',4);
         //add two to the cart for the second product
-        cy.get('.products').find('.product').eq(2);
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART');
 
 
     } )
