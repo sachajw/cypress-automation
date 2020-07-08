77941410c0c5598a232c424334fd6995cf6839b8
Sacha Wharton <sacha@gmail.com>
2020-07-08 15:18:46 +0200

    Using fixtures for pre-populating data via a json file

--- cypress/integration/examples/09-test-spec-framework.js
+++ cypress/integration/examples/09-test-spec-framework.js
@@ -4,15 +4,16 @@ describe('before after test one', function()
 {
     before(function() {
         //runs once before all tests in the block
-
+        cy.fixture('example').then(function(data)
+        { //this is a global variable and makes the data available everywhere
+this.data=data;
+        });
     });
 
 it('before after test one',function() {
     cy.visit('https://rahulshettyacademy.com/angularpractice/');
-    
-    cy.get('input[name="name"]:nth-child(2)').type('Bob');
-    cy.get('select').select('Female');
-
+    //this is a global variable and makes the data available everywhere and that is how you call it
+    cy.get('input[name="name"]:nth-child(2)').type(this.data.name);
+    cy.get('select').select(this.data.gender);
 });
-
 });
