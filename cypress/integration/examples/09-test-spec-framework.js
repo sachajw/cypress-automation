a8e76373c1c0ca5da4601a3956476bfede2f85f3
Sacha Wharton <sacha@gmail.com>
2020-07-08 17:32:55 +0200

    Optimising tests with classes and the page object model

--- cypress/integration/examples/09-test-spec-framework.js
+++ cypress/integration/examples/09-test-spec-framework.js
@@ -1,4 +1,6 @@
 /// <reference types="Cypress" />
+import HomePage from '../pageObjects/HomePage';
+import ProductsPage from '../pageObjects/ProductsPage';
 
 describe('fixtures and before', function() 
 {
@@ -11,21 +13,37 @@ this.data=data;
     });
 
 it('fixtures',function() {
+    //Javascript memory operator
+    const homePage=new HomePage();
+    const productsPage=new ProductsPage();
+
     cy.visit('https://rahulshettyacademy.com/angularpractice/');
-    //this is a global variable and makes the data available everywhere and that is how you call it
-    cy.get('input[name="name"]:nth-child(2)').type(this.data.name);
-    cy.get('select').select(this.data.gender);
-    cy.get(':nth-child(4) > .ng-untouched').should('have.value',this.data.name);
-    //validating attributes
-    cy.get('input[name="name"]:nth-child(2)').should('have.attr','minlength','2');
-    cy.get('#inlineRadio3').should('be.disabled');
-    cy.get(':nth-child(2) > .nav-link').click();
-    //use an array in your support/command.js file to specify multiple objects/products
-    //custom cypress cmd created in command.js - dynamic data and not hardcoded
+    
+    homePage.getEditBox().type(this.data.name);
+    homePage.getGender().select(this.data.gender);
+    homePage.getTwoWayDataBinding().should('have.value',this.data.name);
+    homePage.getEditBox().should('have.attr','minlength','2');
+    homePage.getEntrepreneur().should('be.disabled');
+    homePage.getShopTab().click();
+
     this.data.productName
     
     this.data.productName.forEach(function(element) {
         cy.selectProduct(element);        
     });
+    productsPage.checkOutButton().click();
+    
+    //this is a global variable and makes the data available everywhere and that is how you call it
+    //cy.get('input[name="name"]:nth-child(2)').type(this.data.name);
+    //cy.get('select').select(this.data.gender);
+    //cy.get(':nth-child(4) > .ng-untouched').should('have.value',this.data.name);
+    //validating attributes
+    //cy.get('input[name="name"]:nth-child(2)').should('have.attr','minlength','2');
+    //cy.get('#inlineRadio3').should('be.disabled');
+    //pause is great for debugging a troublesome step
+    //cy.pause();
+    //cy.get(':nth-child(2) > .nav-link').click();
+    //use an array in your support/command.js file to specify multiple objects/products
+    //custom cypress cmd created in command.js - dynamic data and not hardcoded
     });
 });
---
+++ cypress/integration/pageObjects/ProductsPage.js
@@ -0,0 +1,13 @@
+class productsPage
+{
+
+checkOutButton()
+{
+    return cy.get('#navbarResponsive > .navbar-nav > .nav-item > .nav-link');
+}
+
+
+
+}
+
+export default productsPage;
