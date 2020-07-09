b7949b4be93d62bc24185b5434a6a84622042cbc
Sacha Wharton <sacha@gmail.com>
2020-07-09 16:45:08 +0200

    stubbing an error message

--- cypress/integration/examples/faketest.js
+++ cypress/integration/examples/faketest.js
@@ -1,13 +1,27 @@
 /// <reference types="Cypress" />
 
+const { delay } = require("cypress/types/bluebird");
+
 describe('API fake Test', function()
 {
     it('My FirstTest case',function(){
-
+        cy.visit('https://example.cypress.io/commands/network-requests')
         cy.server()
 
-        cy.route()
+        cy.route(
+            {
+                method: 'PUT',
+                url: 'comments/*',
+                status: 404,
+                response: {
+                    error : "comment does not exist"
+                },
+                delay: 1000
+}).as('UpdateComment')
 
+cy.get('.network-put').click()
+cy.get('.network-put-comment').should('contain',"comment does not exist");
 
-    });
+});
+
 });
